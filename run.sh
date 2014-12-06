87e5600787600a2762f783a5f9d1e23296044d91
jankuca <jan@jankuca.com>
2014-12-06 12:09:47 +0100

    pass run.sh arguments to the app

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 
-./build.sh $@ || exit 1
+./build.sh || exit 1
 
 
 source "./config.sh"
 
 
 echo -e "\033[0;36mRunning app...\033[0m"
-"./$app_bin_path" || {
+"./$app_bin_path" $@ || {
   echo -e "\033[0;31merror\033[0m"
   exit 1
 }
